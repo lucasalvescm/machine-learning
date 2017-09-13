dd5899fc9d9a1e337559b8f99aedbfb5a25ae69f
lucasalvescm <lucasalves.s@outlook.com>
2017-09-13 17:34:46 -0300

    New

--- floods.py
+++ floods.py
@@ -1,11 +1,51 @@
 import MySQLdb
+from datetime import datetime
 # Open database connection
 db = MySQLdb.connect("localhost","root","root","flood_db" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 # execute SQL query using execute() method.
+dict_months={
+    'Jan':1,
+    'Feb':2,
+    'Mar':3,
+    'Apr':4,
+    'May':5,
+    'Jun':6,
+    'Jul':7,
+    'Aug':8,
+    'Sep':9,
+    'Oct':10,
+    'Nov':11,
+    'Dec':12,
+}
 cursor.execute("SELECT * FROM floods;")
 data = list(cursor.fetchall())
+d2 = dict((k, v) for k, v in dict_months.items())
 for d in data:
-    print("Latitude",d[19])
-    print("Longitude",d[20])
+
+    data = str(d[9]).split('-') if d != '#N/DISP' else ''
+
+    try:
+        #import ipdb;ipdb.set_trace()
+        day = data[0]
+        month = d2[str(data[1])]
+        year = '19'+str(data[2]) if (int(data[2]) > 84 and int(data[2]) <= 99) else '20'+str(data[2])
+        date = str(year)+'-'+str(month)+'-'+str(day)
+        import ipdb;ipdb.set_trace()
+        date = datetime.strptime(date, '%Y-%m-%d')
+
+
+
+        cursor.execute("UPDATE floods SET Began='{}' WHERE Register='{}'".format(str(date),int(d[0])))
+
+        cursor.fetchall
+
+
+    except:
+        break
+
+
+
+
+
