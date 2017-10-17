2ae762c545915d232eb77ba8b1a23586fbe21911
lucasalvescm <lucasalves.s@outlook.com>
2017-10-17 16:58:16 -0200

    Plotando graficos

--- testando_irirs.py
+++ testando_irirs.py
@@ -14,37 +14,47 @@ import json
 from calculos import AjusteCurva
 
 
-nome_algoritmo = ''
-inicio = time.time()
-# Function to get data
-def get_data(file_name):
-    '''
-    Método que separa a metade do csv para treinamento e a outra para testar as predições.
-    Utiliza três variaveis(temperatura minima, preciptação e velocidade do vento) e o boolean de chuva.
-    '''
-    data = pd.read_csv(file_name)
+# nome_algoritmo = ''
+# inicio = time.time()
+# # Function to get data
+# def get_data(file_name):
+#     '''
+#     Método que separa a metade do csv para treinamento e a outra para testar as predições.
+#     Utiliza três variaveis(temperatura minima, preciptação e velocidade do vento) e o boolean de chuva.
+#     '''
+#     data = pd.read_csv(file_name)
 
-    listx = []
-    listy=[]
-    #Dados da primeira linha até 1373
-    for sepal_length,sepal_width in zip(data['sepal_length'],data['sepal_width']):
-        listx.append(sepal_length)
-        listy.append(sepal_width)
+#     listx = []
+#     listy=[]
+#     #Dados da primeira linha até 1373
+#     for sepal_length,sepal_width in zip(data['sepal_length'],data['sepal_width']):
+#         listx.append(sepal_length)
+#         listy.append(sepal_width)
         
 
-    return listx,listy
+#     return listx,listy
 
 
-x, y = get_data('iris_setosa.csv')
+# x, y = get_data('iris_setosa.csv')
 
-b0,b1,sum_y_square, sum_y = AjusteCurva.modelo_mmq(x,y)
+# b0,b1,sum_y_square, sum_y = AjusteCurva.modelo_mmq(x,y)
 
-desvio = AjusteCurva.desvio(b0,b1,x,y)
+# desvio = AjusteCurva.desvio(b0,b1,x,y)
 
-coeficiente = AjusteCurva.coeficiente_determinacao(desvio,sum_y_square,sum_y,len(x))
+# coeficiente = AjusteCurva.coeficiente_determinacao(desvio,sum_y_square,sum_y,len(x))
 
-variancia = AjusteCurva.variancia_residual(desvio,len(x))
+# variancia = AjusteCurva.variancia_residual(desvio,len(x))
 
-print(desvio, coeficiente, variancia)
+# print(desvio, coeficiente, variancia)
 
+
+listx = [3.8,3.4,3.2,4,3.2]
+listy=[5.1,5,4.4,5.8,4.7]
+listp=[3.49,3.41,2.9,4.07,3.16]
+
+plt.plot(listx,listy,'ro')
+plt.plot(listp,listy,'bo')
+
+
+plt.show()
 
