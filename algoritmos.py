3d7fb2b74a08742bdf17447387ef3c1effdec3ba
Lucas Alves <lucasalves.s@outlook.com>
2017-10-22 22:14:19 -0200

    Organizando código e gerando graficos

--- algoritmos.py
+++ algoritmos.py
@@ -5,16 +5,17 @@ import numpy as np
 import pandas as pd
 from sklearn import datasets, linear_model,svm
 from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsClassifier
 import os
 import csv
 from itertools import product 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 import time
 import json
 from calculos import AjusteCurva
 from matplotlib.pyplot import step, xlim, ylim, show
+from sklearn.metrics import r2_score
 
 class MachineLearning:
     def get_data(self,file_name):
@@ -32,16 +33,17 @@ class MachineLearning:
             #Dados da primeira linha até 1373
             for climate_data,flood in zip(data['json_data'],data['flood']):
                 #import ipdb;ipdb.set_trace()
-                x = climate_data.replace("'",'"')
-                try:
-                    json_after = json.loads(x)
-                    if json_after.get('history'):
-                        json_dailly = json_after['history']['dailysummary']
-                        if json_dailly and json_dailly[0]['mintempm'] != '-55573':
-                            climate_data_fit.append(climate_data)
-                            flood_data_fit.append(flood)   
-                except Exception as e:
-                    print(str(e))
+                if flood==1:
+                    x = climate_data.replace("'",'"')
+                    try:
+                        json_after = json.loads(x)
+                        if json_after.get('history'):
+                            json_dailly = json_after['history']['dailysummary']
+                            if json_dailly and json_dailly[0]['mintempm'] != '-55573':
+                                climate_data_fit.append(climate_data)
+                                flood_data_fit.append(flood)   
+                    except Exception as e:
+                        print(str(e))
             return climate_data_fit,flood_data_fit
     def calculando_ajuste_curva(self,list_var,content):
         dict_result = {}
@@ -257,7 +259,7 @@ class MachineLearning:
             except:
                 pass
 
-        model = DecisionTreeClassifier()
+        #model = DecisionTreeClassifier()
         #model = KNeighborsClassifier()
         #model = svm.SVC()
         #model = GaussianNB()
@@ -318,7 +320,8 @@ class MachineLearning:
                 print(list_temp)
                 if list_temp:
                     #import ipdb;ipdb.set_trace()
-                    predicted = model.predict(np.array([list_temp]))
+                    #predicted = model.predict(np.array([list_temp]))
+                    predicted
                     if predicted == flood[cont]:
                         acertos +=1
                     else:
@@ -361,65 +364,233 @@ class MachineLearning:
                         print(dupla)
                         resultado = self.calculando_ajuste_curva(dupla,content_data)
                         writer.writerow({'variaveis': resultado[0],'desvio': resultado[1],'coeficiente': resultado[2],'variancia': resultado[3]})
+
+    def get_model_mmq(self,x,y):
+            b0,b1,sum_y_square, sum_y = AjusteCurva.modelo_mmq(x,y)
+
+            desvio = AjusteCurva.desvio(b0,b1,x,y)
+
+            coeficiente = AjusteCurva.coeficiente_determinacao(desvio,sum_y_square,sum_y,len(x))
+
+            variancia = AjusteCurva.variancia_residual(desvio,len(x))
+
+            print(desvio, coeficiente, variancia)
+            return b0,b1
+
+    def treinando_variaveis(self,variaveis,data,flood):
+        cont = 0
+        list_all = []
+        list_flood = []
+        count = 0
+
+        num_train = int((len(data)*0.9))
+        #import ipdb;ipdb.set_trace()
+        num_test = num_train+1
+        #import ipdb;ipdb.set_trace()
+        parametros_1 = []
+        parametros_2 = []
+        for dt in data[:num_train]:
+            try:
+
+                json_data = dt.replace("'",'"')
+                json_content = json.loads(json_data)
+
+                json_dailly = json_content['history']['dailysummary']
+                #import ipdb;ipdb.set_trace()
+                # parametros_1 = []
+                # parametros_2 = []
+                
+                if json_dailly != [] and flood[count] == 1:
+                    try:
+                        #import ipdb;ipdb.set_trace()
+                        for key in json_dailly[0].keys():
+                            if key in variaveis:
+                                #print(key)
+                                try:
+                                    param = float(json_dailly[0][key])
+                                except:
+                                    param = 0
+                                if param == '':
+                                    param = 0
+                                #print(param)
+                                if key == variaveis[0]:
+                                    parametros_1.append(param)
+                                if key == variaveis[1]:
+                                    
+                                    parametros_2.append(param)
+                                
+                        cont+=1
+                    except Exception as e:
+                        print(e)
+                        import ipdb;ipdb.set_trace()
+                    #import ipdb;ipdb.set_trace()
+                    list_all.append(list_temp)
+                count+=1
+
+            except:
+                pass
+        #CLASSIFICACAO        
+        #model = DecisionTreeClassifier()
+        #model = KNeighborsClassifier()
+        #model = svm.SVC()
+        #model = GaussianNB()
+        #model = LogisticRegression()
+
+        #REGRESSAO
+        model = LinearRegression()
+        #model = DecisionTreeRegressor()
+
+        nome_algoritmo = model.__class__.__name__
+        #import ipdb;ipdb.set_trace()
+        #model.fit(np.transpose(np.matrix(parametros_1)),np.transpose(np.matrix(parametros_2)))
+        model.fit(np.transpose(np.matrix(parametros_1)),np.transpose(np.matrix(parametros_2)))
+
+        m = MachineLearning()
+
+        #b0,b1 = m.get_model_mmq(parametros_1,parametros_2)
+        #import ipdb;ipdb.set_trace()
+        result = []
+        #print('##### FIT ######')
+        num = 0
+
+        result = []
+        list_all = []
+        list_flood = []
+        
+        json_data = ''
+        json_dailly = ''
+        json_content = ''
+        param = ''
+        list_temp = []
+        acertos = 0
+        erros = 0
+        cont = 0
+        cont_itens = num_test
+        for dt in data[num_test:]:
+
+            try:
+                #import ipdb;ipdb.set_trace()
+                parametros_base = []
+                parametros_reais = []
+                json_data = dt.replace("'",'"')
+                json_content = json.loads(json_data)
+
+                json_dailly = json_content['history']['dailysummary']
+                #import ipdb;ipdb.set_trace()
+                list_temp = []
+                # if json_dailly:
+                #     try:
+               
+                if json_dailly:
+                    try:
+                        for key_ in json_dailly[0].keys():
+                            #print(key_)
+                            #import ipdb;ipdb.set_trace()
+                            #if key_ in ('mintempm','minhumidity','minpressurem','maxwspdi','precipi'):
+                            if key_ in variaveis:
+                                #import ipdb;ipdb.set_trace()
+                                try:
+                                    param = float(json_dailly[0][key_])
+                                except:
+                                    param = float(0)
+                                if param == '':
+                                    param = float(0)
+                                if key_ == variaveis[0]:
+                                    parametros_base.append(param)
+                                if key_ == variaveis[1]:
+                                    parametros_reais.append(param)
+                                    
+                    except Exception as e:
+                        print(e)
+                       
+                if parametros_base:
+                    #import ipdb;ipdb.set_trace()
+                    predicted = model.predict(np.array([parametros_base]))
+                    #predicted = b0 + (b1*float(parametros_base[0]))
+                    # print(parametros_base)
+                    # print(parametros_reais)
+                    # print(predicted)
+                    
+                    result.append([parametros_base,parametros_reais,predicted]) # .append(nº da linha .csv, boolean real, boolean previsto)
+                #import ipdb;ipdb.set_trace()
+                num+=1
+                cont+=1
+                cont_itens+=1
+            except Exception as e:
+                print(str(e))
+                pass
+
+        return result,nome_algoritmo
+
+    def salvando_graficos(self,nome_pasta,grafico_valores_base,grafico_valores_reais,grafico_valores_previstos,parametros):
+            fig = plt.figure(figsize=(12, 7.195), dpi=100)
+            nome_arq = 'graficos/'+nome_pasta+'/'+str(parametros)+'.png'
+
+            plt.plot(grafico_valores_base,grafico_valores_reais,'bo',label='Reais')
+            plt.plot(grafico_valores_base,grafico_valores_previstos,'ro',label='Previstos')
+
+
+
+          
+            
+            plt.ylabel(dict_for_name.get(str(parametros[0])))
+            plt.xlabel(dict_for_name.get(str(parametros[1])))
+            plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
+            fig.savefig(nome_arq, dpi=500, format='png')
+            #plt.show()
+
+  
+        
   
 mac_lg = MachineLearning()
 # mac_lg.calcular_ajustes_curva_csv()
-lista_parametros,flood = mac_lg.get_data('data_bases/America/Chicago.csv')         
-resultado, nome_algoritmo,acertos,erros = mac_lg.treinamento_csv(lista_parametros,flood)           
-print('===============================')
-#import ipdb;ipdb.set_trace()
-lista_previsao = []
-lista_real = []
-for res in resultado:
+dados,flood = mac_lg.get_data('data_bases/America/Anchorage.csv')         
+# resultado, nome_algoritmo,acertos,erros = mac_lg.treinamento_csv(lista_parametros,flood)
+list_param = ['maxtempm','mintempm','minhumidity','maxhumidity','maxpressurem','minpressurem','maxwspdm','precipm']
+#list_param = ['mintempm','minhumidity','minpressurem','maxwspdi','precipi']
+
+dict_for_name = {
+    'maxtempm': 'TEMPERATURA MAX.',
+    'mintempm': 'TEMPERATURA MIN.',
+    'minhumidity': 'UMIDADE MIN.',
+    'maxhumidity': 'UMIDADE MAX.',
+    'maxpressurem': 'PRESSAO MAX.',
+    'minpressurem': 'PRESSAO MIN.',
+    'maxwspdm': 'VELOC. VENTO',
+    'precipm': 'PRECIPTACAO',
+
+
+
+}
+list_combinado = list(product(list_param,repeat=2))
+for parametros in list_combinado:
+    resultado, nome_algoritmo = mac_lg.treinando_variaveis(parametros,dados,flood)       
+    print('===============================')
     #import ipdb;ipdb.set_trace()
-    tamanho_lista = len(lista_previsao)-1
-    #print(tamanho_lista)
-    if tamanho_lista>0:
-        if (res[2] != lista_previsao[tamanho_lista]):
-            lista_previsao.append(int(res[2]))
-            lista_real.append(int(res[1])) 
-        else:
-            pass
-    else:
-        lista_previsao.append(int(res[2]))
-        lista_real.append(int(res[1]))
+    lista_previsao = []
+    lista_real = []
+    grafico_valores_previstos = []
+    grafico_valores_reais = []
+    grafico_valores_base = []
+    for res in resultado:
+        #import ipdb;ipdb.set_trace()
+        # if res[1][0] > 7:
+        #     import ipdb;ipdb.set_trace() 
+        grafico_valores_previstos.append(res[2][0])
+        grafico_valores_reais.append(res[1][0])
+        grafico_valores_base.append(res[0][0])
 
+        #print(res)
+    coef = r2_score(grafico_valores_reais,grafico_valores_previstos)  
+    print(dict_for_name.get(str(parametros[0])))
+    print(dict_for_name.get(str(parametros[1])))
+    print(coef)
 
-x = np.arange(0, len(lista_real))
+    mac_lg.salvando_graficos('anchorage',grafico_valores_base,grafico_valores_reais,grafico_valores_previstos,parametros)
 
-y = np.array(lista_real)
-y2 = np.array(lista_previsao)
-
-
-xlim(0, len(lista_real))
-#ylim(-0.5, 1.5)
-step(x, y)
-
-show()
-
-step(x, y2)
-show()
-
-
-# plt.ylabel('Pétala Width')
-# plt.xlabel('Pétala Length')
-# plt.legend(bbox_to_anchor=(1, 0.18), loc=1, borderaxespad=0.)
-
-
-
-print("ACERTOS: "+str(acertos))
-print("ERROS: "+str(erros))
-
-
-
-
-
-# ACERTOS: 2671
-# ERROS: 1805
-
-
-
-
+   
+    
+    
 
 
 
